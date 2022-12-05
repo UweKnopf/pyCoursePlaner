54fd6402f702c56732993bccc8ab17a836bbcb89
UweKnopf <69891951+UweKnopf@users.noreply.github.com>
2022-12-05 23:09:41 +0100

    tk_scroll implemented

--- src/pyCoursePlaner/ui.py
+++ src/pyCoursePlaner/ui.py
@@ -1,6 +1,8 @@
 from tkinter import * #still unsure about tkinter but sticking with it for now (https://github.com/TomSchimansky/CustomTkinter looks cool)
 #example
 from dnd_res import *
+from scrollable_widget import *
+from tk_scroll import *
 draggable_places_list = []
 #TODO: highlighting dragable places when hovering over them with a dragable
 
@@ -19,6 +21,34 @@ def scroll_bar_init(root):
     root.grid_rowconfigure(3, weight=1)
     root.grid_columnconfigure(3, weight=1)
 
+def scroll_bar_widget(root):
+    scrollFrame = ScrollFrame(root)
+    #scrollFrame.grid_rowconfigure(3, weight=1)
+    f = []
+    for row in range(40):
+            a = row
+            f.append(Label(scrollFrame.viewPort, bd=5, bg="black", height=5, width=5).grid(row=row, column=1,pady=3))#scrollFrame.viewport return NoneType
+            
+            t="this is the second column for row %s" %row
+            #make_draggable(b)
+
+            #tk.Button(scrollFrame.viewPort, text=t, command=lambda x=a: printMsg("Hello " + str(x))).grid(row=row, column=1)
+
+        # when packing the scrollframe, we pack scrollFrame itself (NOT the viewPort)
+    testF = Label(scrollFrame, bd=5, bg="black", height=5, width=5).grid(row=2, column=3,pady=3)
+    scrollFrame.pack(side="left", fill="y", expand=False)
+    make_draggable(testF)
+    def printMsg(msg):
+        print(msg)
+    #scrollFrame.pack(side="left", fill="both")
+
+
+
+
+
+
+
+
 def planner_frame_init(root):
     planner_frame = Frame(root, bd=1, relief="solid")
     #planner_frame.pack(side=RIGHT, fill=BOTH)
@@ -31,11 +61,13 @@ def planner_frame_init(root):
     frame.pack(side=LEFT)
     planner_frame.pack(side=LEFT, fill="both", expand=True)
     planner_frame.update()
+
 root = Tk()
 root.geometry("1000x600")
-scroll_bar_init(root)
+scroll_bar_widget(root)
 planner_frame_init(root)
-#scroll_bar_init(root)
-root.update()
+#scroll_bar_widget(root)
 
+
+root.update()
 mainloop()
